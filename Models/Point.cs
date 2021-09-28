e51aa9a779af6e10e7265a56def269885195491a
Anna Hakobyan <Anna.Hakobyan@helpsystems.com>
2021-09-29 03:00:49 +0400

    Add project files.

---
+++ Models/Point.cs
@@ -0,0 +1,26 @@
+﻿using GCForSummerSchool.Interfaces;
+using System;
+
+namespace GCForSummerSchool.Models
+{
+    public class Point : IPoint
+    {
+        // Constructor:
+        public Point(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        // Property implementation:
+        public int X { get; set; }
+
+        public int Y { get; set; }
+
+        // Property implementation
+        public double Distance =>
+           Math.Sqrt(X * X + Y * Y);
+
+        public override string ToString() => $"X is {X}, Y is {Y}";
+    }
+}
